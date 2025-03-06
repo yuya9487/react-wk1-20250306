fb85891a13e16d1690409b0c3403d59bbd0a66b3
yuya9487 <131440862+yuya9487@users.noreply.github.com>
2025-03-06 15:14:22 +0800

    className
    transfer class to className

--- src/App.jsx
+++ src/App.jsx
@@ -24,83 +24,83 @@ function App() {
             </nav>
         </header>
 
-        <div class="main_block">
-            <div class="main_background main">
-                <div class="main_words">
-                    <p class="redline"></p>
-                    <p class="H1">ROCK YOUR LIFE<br/>with <span class="H1 RedWord">ONE OK ROCK</span></p>
-                    <img class="ROCK_pic" src="img/ROCK.png"/>
+        <div className="main_block">
+            <div className="main_background main">
+                <div className="main_words">
+                    <p className="redline"></p>
+                    <p className="H1">ROCK YOUR LIFE<br/>with <span className="H1 RedWord">ONE OK ROCK</span></p>
+                    <img className="ROCK_pic" src="img/ROCK.png"/>
                 </div>
             </div>
         </div>
         
         
-        <div class="title_place" id="NEWSpart">
-            <p class="title H2">NEWS</p>
+        <div className="title_place" id="NEWSpart">
+            <p className="title H2">NEWS</p>
         </div>
         
     
-        <div class="part1">
-            <img class="album_pic" src="img/專輯.jpg"/>
-            <div class="album_block">
-                <p class="H1">NEW ALBUM</p>
-                <p class="T1">第十章專輯《Luxury Disease》現正發行中！</p>
-                <a href="https://oor.lnk.to/LDJAW" class="p1_RM T1">read more {'>'}</a>
+        <div className="part1">
+            <img className="album_pic" src="img/專輯.jpg"/>
+            <div className="album_block">
+                <p className="H1">NEW ALBUM</p>
+                <p className="T1">第十章專輯《Luxury Disease》現正發行中！</p>
+                <a href="https://oor.lnk.to/LDJAW" className="p1_RM T1">read more {'>'}</a>
             </div>
         </div>
     
-        <div class="part2">
-            <div class="p2_box">
-                <p class="H2 p2_word">Luxury Disease Asia Tour 2023 巡迴中</p>
-                <img class="tour_pic" src="img/TOUR.png"/>
-                <a href="https://www.oneokrock.com/en/tour/" class="p2_RM T1">read more {'>'}</a>
+        <div className="part2">
+            <div className="p2_box">
+                <p className="H2 p2_word">Luxury Disease Asia Tour 2023 巡迴中</p>
+                <img className="tour_pic" src="img/TOUR.png"/>
+                <a href="https://www.oneokrock.com/en/tour/" className="p2_RM T1">read more {'>'}</a>
             </div>
         </div>
     
-        <div class="part3">
-            <div class="title_place">
-                <p class="title_MV H2">POPULAR MV</p>
+        <div className="part3">
+            <div className="title_place">
+                <p className="title_MV H2">POPULAR MV</p>
             </div>
-            <div class="MV_blocks">
-                <div class="MV1 MV_box">
-                    <img class="MV_pic" src="img/The Beginning.jpg"/>
-                    <p class="songname T1">The Beginning</p>
-                    <a href="https://youtu.be/Hh9yZWeTmVM?si=fS5vVh0vh7Ihh0zU" class="watchnow T1">watch now {'>'}</a>
+            <div className="MV_blocks">
+                <div className="MV1 MV_box">
+                    <img className="MV_pic" src="img/The Beginning.jpg"/>
+                    <p className="songname T1">The Beginning</p>
+                    <a href="https://youtu.be/Hh9yZWeTmVM?si=fS5vVh0vh7Ihh0zU" className="watchnow T1">watch now {'>'}</a>
                 </div>
                 
-                <div class="MV2 MV_box">
-                    <img class="MV_pic" src="img/Clock Strikes.jpg"/>
-                    <p class="songname T1">Clock Strikes</p>
-                    <a href="https://youtu.be/6YZlFdTIdzM?si=uYpUqqeny83Vu2NL" class="watchnow T1">watch now {'>'}</a>
+                <div className="MV2 MV_box">
+                    <img className="MV_pic" src="img/Clock Strikes.jpg"/>
+                    <p className="songname T1">Clock Strikes</p>
+                    <a href="https://youtu.be/6YZlFdTIdzM?si=uYpUqqeny83Vu2NL" className="watchnow T1">watch now {'>'}</a>
                 </div>
     
-                <div class="MV3 MV_box">
-                    <img class="MV_pic" src="img/完全感覚Dreamer.png"/>
-                    <p class="songname T1">完全感覚Dreamer</p>
-                    <a href="https://youtu.be/NWDAjOsTYC8?si=hP870FmyuMtPI-bc" class="watchnow T1">watch now {'>'}</a>
+                <div className="MV3 MV_box">
+                    <img className="MV_pic" src="img/完全感覚Dreamer.png"/>
+                    <p className="songname T1">完全感覚Dreamer</p>
+                    <a href="https://youtu.be/NWDAjOsTYC8?si=hP870FmyuMtPI-bc" className="watchnow T1">watch now {'>'}</a>
                 </div>
             </div>
-            <div class="morevedio_place">
-                <button class="morevedio T1_B" type="button">
-                    <a class="a" href="https://www.youtube.com/@ONEOKROCK/releases">MORE VIDEO {'>'}</a>
+            <div className="morevedio_place">
+                <button className="morevedio T1_B" type="button">
+                    <a className="a" href="https://www.youtube.com/@ONEOKROCK/releases">MORE VIDEO {'>'}</a>
                 </button>
             </div>
             
         </div>
     
         <footer>
-            <p class="F_word T2">© 10969 UNOFFICIAL FAN CLUB, Inc.</p>
-            <div class="buttons_place">
-                <button type="button" class="img_btn">
-                    <a href="https://www.facebook.com/ONEOKROCK"><img class="fb icon_btn" src="img/fb.png" alt="fb"/></a>
+            <p className="F_word T2">© 10969 UNOFFICIAL FAN CLUB, Inc.</p>
+            <div className="buttons_place">
+                <button type="button" className="img_btn">
+                    <a href="https://www.facebook.com/ONEOKROCK"><img className="fb icon_btn" src="img/fb.png" alt="fb"/></a>
                 </button>
     
-                <button type="button" class="img_btn">
-                    <a href="https://www.instagram.com/oneokrockofficial/"><img class="ig icon_btn" src="img/ig.png" alt="ig"/></a>
+                <button type="button" className="img_btn">
+                    <a href="https://www.instagram.com/oneokrockofficial/"><img className="ig icon_btn" src="img/ig.png" alt="ig"/></a>
                 </button>
     
-                <button type="button" class="img_btn">
-                    <a href="https://twitter.com/ONEOKROCK_japan"><img class="x icon_btn" src="img/x.png" alt="x"/></a>
+                <button type="button" className="img_btn">
+                    <a href="https://twitter.com/ONEOKROCK_japan"><img className="x icon_btn" src="img/x.png" alt="x"/></a>
                 </button>
             </div>
         </footer>
